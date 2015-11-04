081831f8b5974677811dfccc8f745647c47396fb
maohule <932604487@qq.com>
2015-11-04 12:13:19 +0800

    add distributed

--- A2_30.java
+++ A2_30.java
@@ -1,4 +1,4 @@
-package edu.xupt.java.homework.chp2;
+﻿package edu.xupt.java.homework.chp2;
 
 import java.util.Scanner;
 
@@ -6,7 +6,7 @@ public class A2_30 {
 	public static void main(String[] args) {
 		int number=0,d5,d4,d3,d2,d1;
 		Scanner in=new Scanner(System.in);
-		System.out.println("请输入一个5位数字：");
+		System.out.println("请您输入一个5位数字：");
 		number=in.nextInt();
 		d5=number/10000;
 		d4=number%10000/1000;
